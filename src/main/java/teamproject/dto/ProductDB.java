0c7fae09c83dde9287e5a43bc8e92191b6953644
realmer96 <rlatmdxo50@gmail.com>
2021-01-27 20:50:35 +0900

    제품 Dto

--- src/main/java/teamproject/dto/ProductDB.java
+++ src/main/java/teamproject/dto/ProductDB.java
@@ -1,7 +1,5 @@
 package teamproject.dto;
 
-import org.springframework.web.multipart.MultipartFile;
-
 public class ProductDB {
 	private int productCode;
 	private String productName;
@@ -9,6 +7,43 @@ public class ProductDB {
 	private int productPrice;
 	private String productImagePath;
 	private String lowcategoryEng;
+	
+	public int getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(int productCode) {
+		this.productCode = productCode;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+	public int getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(int productPrice) {
+		this.productPrice = productPrice;
+	}
+	public String getProductImagePath() {
+		return productImagePath;
+	}
+	public void setProductImagePath(String productImagePath) {
+		this.productImagePath = productImagePath;
+	}
+	public String getLowcategoryEng() {
+		return lowcategoryEng;
+	}
+	public void setLowcategoryEng(String lowcategoryEng) {
+		this.lowcategoryEng = lowcategoryEng;
+	}
 	
 	
 }
